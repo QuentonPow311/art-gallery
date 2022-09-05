c5b16a06d3a9f4924efb76ce08fc959529eb98b7
QuentonPow311 <quenpowell@gmail.com>
2022-09-05 13:36:57 -0400

    finsh

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import { useEffect } from 'react';
+import { useSelector, useDispatch, connect } from 'react-redux'
+import {fetchData, nextImage, prevImage, reset, setArtId} from './features/dataSlice';
+
+const mapStateToProps = (state) => ({
+  artId: state.data.artId
+})
+
+
+function App(props) {
+  // your logic goes here!
+  const dispatch = useDispatch();
+  const currentState = useSelector(state => state.data)
+
+  useEffect(() => {
+    dispatch(fetchData())
+  },[props.artId, dispatch])
+
+  const renderImage = () => {
+  return currentState.apiData ? 
+      <img src ={currentState.apiData.primaryImage} /> :
+      <h3> No Image Found</h3>
+    }
+
+  
+
+  return (
+    <div className="App">
+      <div>
+        <button onClick={() => {dispatch(fetchData())}}>Trigger Thunk</button>
+        <button onClick={() => {}}>Clear</button>
+        <button onClick={() => {dispatch(nextImage())}}>Next</button>
+        <button onClick={() => {dispatch(prevImage())}}>Back</button>
+      </div>
+      <h1>{currentState.artId}</h1>
+      <input onChange={(e) => { }} />
+      <div>
+        {renderImage()} 
+      </div>
+    </div>
+  );
+}
+
+export default connect(mapStateToProps) (App);
